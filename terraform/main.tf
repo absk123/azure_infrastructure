892cce8a5303b3604633af168d468406adf70445
Abhisek <absk.sing@outllok.com>
2025-12-27 12:39:56 +0530

    update root moule source uri

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,48 +2,48 @@
 
 
 module "RG" {
-  source = "git::https://github.com/absk123/resource_group.git//azure_resource_group?ref=main"
+  source = "git::https://github.com/absk123/azure_module.git//resource_group/azure_resource_group?ref=main"
   rg     = var.rg
 }
 
 module "SA" {
-  source = "git::ssh://git@github.com/absk123/storage_account.git//azure_storage_account?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//storage_account/azure_storage_account?ref=main"
   sa     = var.sa
   gg     = module.RG.Resource_Group
 }
 
 module "VNET" {
-  source = "git::ssh://git@github.com/absk123/virtual_network.git//azure_virtual_network?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//virtual_network/azure_virtual_network?ref=main"
   nets   = var.nets
   rg     = module.RG.Resource_Group
 }
 
 module "WEB" {
-  source = "git::ssh://git@github.com/absk123/app_service.git//azure_app_service?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//app_service/azure_app_service?ref=main"
   web    = var.web
   rg     = module.RG.Resource_Group
 }
 
 module "AKS" {
-  source = "git::ssh://git@github.com/absk123/aks_cluster.git//azure_aks_cluster?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//aks_cluster/azure_aks_cluster?ref=main"
   aks    = var.aks
   rg     = module.RG.Resource_Group
 }
 
 module "DB" {
-  source = "git::ssh://git@github.com/absk123/data_base.git//azure_data_base?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//data_base/azure_data_base?ref=main"
   db     = var.db
   rg     = module.RG.Resource_Group
 }
 
 module "PIP" {
-  source = "git::ssh://git@github.com/absk123/public_ip.git//azure_public_ip?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//public_ip/azure_public_ip?ref=main"
   pip    = var.pip
   rg     = module.RG.Resource_Group
 }
 
 module "AGW" {
-  source = "git::ssh://git@github.com/absk123/app_gateway.git//azure_app_gateway?ref=main"
+  source = "git::ssh://git@github.com/absk123/azure_module.git//app_gateway/azure_app_gateway?ref=main"
   agw    = var.agw
   rg     = module.RG.Resource_Group
   sbnet  = module.VNET.Sub_Network
